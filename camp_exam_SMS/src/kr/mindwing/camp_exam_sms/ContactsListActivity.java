3a89d5dc31dc180bfc32444d9a80ee478414535a
mindwing <mindwing@gmail.com>
2015-06-10 06:13:29 +0900

    ready for RecyclerView

--- camp_exam_SMS/src/kr/mindwing/camp_exam_sms/ContactsListActivity.java
+++ camp_exam_SMS/src/kr/mindwing/camp_exam_sms/ContactsListActivity.java
@@ -1,13 +1,28 @@
 package kr.mindwing.camp_exam_sms;
 
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Telephony;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.RecyclerView;
 
 public class ContactsListActivity extends ActionBarActivity {
+
+	RecyclerView rv;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
 		setContentView(R.layout.activity_contacts_list);
+
+		rv = (RecyclerView) findViewById(R.id.recycler_view);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
 	}
 }
