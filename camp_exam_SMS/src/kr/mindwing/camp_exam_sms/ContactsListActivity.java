ed0b888a51cdeacc0dc8170e2415bcc73a648c27
mindwing <mindwing@gmail.com>
2015-06-09 17:29:53 +0900

    init

---
+++ camp_exam_SMS/src/kr/mindwing/camp_exam_sms/ContactsListActivity.java
@@ -0,0 +1,13 @@
+package kr.mindwing.camp_exam_sms;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class ContactsListActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_contacts_list);
+	}
+}
